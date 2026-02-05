43447ec3646325f610de4dfdd77511f50f0cd558
Lenny <desbouislenny@gmail.fr>
2026-02-05 10:11:01 +0100

    check et modif

--- src/builtsin/ft_exit.c
+++ src/builtsin/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: fgarnier <fgarnier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 15:28:43 by fgarnier          #+#    #+#             */
-/*   Updated: 2026/02/05 00:24:51 by fgarnier         ###   ########.fr       */
+/*   Updated: 2026/02/05 10:09:08 by ldesboui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ static int	is_all_digit(char *str)
 
 static void	exit_numeric_error(t_cmd *cmd, char ***env, t_cmd *first)
 {
+	//ft_putstr_fd("exit\n", 2);
 	ft_putstr_fd("minishell: exit: ", 2);
 	ft_putstr_fd(cmd->args[1], 2);
 	ft_putendl_fd(": numeric argument required", 2);
@@ -45,10 +46,15 @@ void	ft_exit(t_cmd *cmd, char ***env, t_cmd *first)
 {
 	__int128_t	res;
 
-	if (cmd->args[1] && cmd->args[2])
+	if (cmd->args[1])
 	{
-		ft_putendl_fd("minishell: exit: too many arguments", 2);
-		return ;
+		if (!is_all_digit(cmd->args[1]))
+			exit_numeric_error(cmd, env, first);
+		if (cmd->args[2])
+		{
+			ft_putendl_fd("minishell: exit: too many arguments", 2);
+			return ;
+		}
 	}
 	else if (!cmd->args[1])
 	{
@@ -56,8 +62,6 @@ void	ft_exit(t_cmd *cmd, char ***env, t_cmd *first)
 		free_cmds(first);
 		exit(0);
 	}
-	if (!is_all_digit(cmd->args[1]))
-		exit_numeric_error(cmd, env, first);
 	res = ft_atoll(cmd->args[1]);
 	if (res > LLONG_MAX || res < LLONG_MIN)
 		exit_numeric_error(cmd, env, first);
